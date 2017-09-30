34ddd711d5491f01478fd543739115d9987739d9
foreverbell <dql.foreverbell@gmail.com>
2017-09-30 23:54:38 +0800

    mysterious.

--- fact-cps/Fact.v
+++ fact-cps/Fact.v
@@ -1,6 +1,7 @@
 (** Illustrate some basic ideas of CPS. *)
 
 Require Import Arith.
+Require Import List.
 
 Fixpoint fact (n : nat) : nat :=
   match n with
@@ -38,6 +39,35 @@ Proof.
   unfold fact_cps.
   intros.
   pose proof (fact_cps_eq_k n (fun v => v)).
+  simpl in H.
+  symmetry. auto.
+Qed.
+
+(** another example to show how continuation is arranged in order. *)
+Fixpoint mysterious_cps' (xs : list nat) (k : list nat -> list nat) : list nat :=
+  match xs with
+  | nil => k nil
+  | x :: ys => mysterious_cps' ys (fun zs => k (x :: zs))
+  end.
+
+Definition mysterious_cps (xs : list nat) : list nat := mysterious_cps' xs (fun v => v).
+
+Lemma mysterious_cps_eq_k :
+  forall (xs : list nat) (k : list nat -> list nat),
+    mysterious_cps' xs k = k xs.
+Proof.
+  induction xs; intros.
+  - simpl; auto.
+  - simpl. rewrite IHxs. auto.
+Qed.
+
+(** all elements are actually applied in order. *)
+Theorem mysterious_cps_eq :
+  forall (xs : list nat), mysterious_cps xs = xs.
+Proof.
+  unfold mysterious_cps.
+  intros.
+  pose proof (mysterious_cps_eq_k xs (fun v => v)).
   simpl in H.
   symmetry. auto.
 Qed.
